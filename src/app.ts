5b82d1b5b40ef2fed67834abed3eb065d03c5138
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2023-12-05 14:13:17 -0600

    MD2: Parte final del modulo - todo funciona correctamente

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,8 @@
-console.log('Hola mundo');
+import { findHeroById } from "./services/hero.service"
+
+
+
+
+const hero = findHeroById(2)
+console.log("🚀 ~ file: app.ts:9:", hero?.name ?? 'No exist hero')
+console.log("🚀 ~ file: app.ts:9:", hero?.owner ?? 'No exist hero')
---
+++ src/data/heroes.ts
@@ -0,0 +1,20 @@
+import { IHeroes } from "../interfaces";
+
+
+export const heroes:IHeroes[] = [
+    {
+        id: 1,
+        name: 'Batman',
+        owner: 'DC'
+    },
+    {
+        id: 2,
+        name: 'Hulk',
+        owner: 'DC'
+    },
+    {
+        id: 3,
+        name: 'Ironman',
+        owner: 'Marvel'
+    },
+]
