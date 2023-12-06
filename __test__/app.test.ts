67593a7b7de4c74e22c1d2a581a6fdde99fb7c2e
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2023-12-05 19:55:08 -0600

    Revert "MD3-55: Configurion ambiente de testing con jest"
    This reverts commit d439eb5ff0e9f7fd13bfb6e33c107dd244d57c69.

--- __test__/app.test.ts
+++
@@ -1,9 +0,0 @@
-describe('Pruebas en el archivo app.ts', () => {
-
-    test('Debe ser exitoso', () => {
-        const expected = true;
-        const result = true;
-        expect(result).toBe(expected)
-    });
-
- })
