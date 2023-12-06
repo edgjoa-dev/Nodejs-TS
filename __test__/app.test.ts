d439eb5ff0e9f7fd13bfb6e33c107dd244d57c69
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2023-12-05 19:51:42 -0600

    MD3-55: Configurion ambiente de testing con jest

---
+++ __test__/app.test.ts
@@ -0,0 +1,9 @@
+describe('Pruebas en el archivo app.ts', () => {
+
+    test('Debe ser exitoso', () => {
+        const expected = true;
+        const result = true;
+        expect(result).toBe(expected)
+    });
+
+ })
