ea2dd8fecba802c4681794d5b0f43de7314927a7
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2023-12-05 20:12:29 -0600

    Sec5-55: Configurion inicial ambiente de testing con jest - ok

---
+++ __test__/app.test.ts
@@ -0,0 +1,8 @@
+describe('Pruebas en archivo app.ts', () => {
+    test('should be true', () => {
+        const expected = true;
+        const result = true;
+
+        expect(result).toBe(expected);
+    });
+})
