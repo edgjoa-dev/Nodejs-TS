1a1055680201764eeb24f78d5e9279249fca4dc0
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2023-12-14 15:16:02 -0600

    Vid-79: appConsla - Crear opciones de entrada en consola como argumentos para yarg

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
 import { yarg } from "./plugins/args.plugin"
 
 
-console.log(process.argv)
-console.log(yarg.b);
+// console.log(process.argv)
+// console.log(yarg.b);
+
+
+(async()=> {
+    await main();
+})();
+
+
+async function main() {
+    console.log(yarg);
+}
--- src/plugins/args.plugin.ts
+++ src/plugins/args.plugin.ts
@@ -2,4 +2,23 @@ import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 
 export const yarg = yargs(hideBin(process.argv))
+  .options('b',{
+      alias: 'base',
+      type: 'number',
+      demandOption: true,
+      describe: 'Número base de la tabla de multiplicar'
+    }
+  )
+  .options('l',{
+    alias: 'limit',
+    type: 'number',
+    default: 10,
+    describe: 'Límite máximo de la tabla de multiplicar'
+  })
+  .options('s',{
+    alias: 'show',
+    type: 'boolean',
+    default: false,
+    describe: 'Mostrar la tabla de multiplicar'
+  })
   .parseSync()
